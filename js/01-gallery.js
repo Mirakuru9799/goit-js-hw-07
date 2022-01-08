673ddb45534a3c0da69613fae5a95c6e8087d14a
Marina Hrytsai <polanic@ukr.net>
2022-01-08 08:02:48 +0200

    Add HW-07

---
+++ js/01-gallery.js
@@ -0,0 +1,54 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryRef = document.querySelector(".gallery");
+let modal;
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class = "gallery__item">
+<a class="gallery__link" onclick="return false" href="${original}">
+<img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+/>
+</a></div>`
+  )
+  .join("");
+
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryRef.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(e) {
+  const isImage = e.target.classList.contains("gallery__image");
+  if (!isImage) {
+    return;
+  }
+
+  createModalImg(e.target.dataset.source);
+  modal.show();
+}
+
+function createModalImg(gallery) {
+  modal = basicLightbox.create(
+    ` <div class="modal">
+    <img src= "${event.target.dataset.source}">
+    </div>`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", onKeyboardClick);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", onKeyboardClick);
+      },
+    }
+  );
+}
+
+function onKeyboardClick(event) {
+  if (event.code === "Escape") {
+    modal.close();
+  }
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,22 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+  `;
+  })
+  .join("");
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryList);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
